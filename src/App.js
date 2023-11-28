c291a402336c2b033b51d9155c93a0fc389b38e8
osman <osmnlhn@gmail.com>
2023-11-28 00:58:24 +0000

    HomePage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import HomePage from './components/HomePage';
+import Homepage from './components/HomePage'
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Homepage/>} />
       </Routes>
     </div>
   );
